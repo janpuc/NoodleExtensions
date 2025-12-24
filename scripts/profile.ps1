bafec112938c63951da806299a08dd2e614ae95d
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-12-24 15:23:04 -0400

    update NDK path reference in scripts

--- scripts/profile.ps1
+++ scripts/profile.ps1
@@ -1,14 +1,15 @@
 #!/usr/bin/env pwsh
 
-if (Test-Path "$PSScriptRoot/ndkpath.txt")
+if (Test-Path "$PSScriptRoot/../ndkpath.txt")
 {
-    $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+    $NDKPath = Get-Content $PSScriptRoot/../ndkpath.txt
 } else {
     $NDKPath = $ENV:ANDROID_NDK_HOME
 }
 
-python3.10 $NDKPath/simpleperf/app_profiler.py -p com.beatgames.beatsaber -a com.unity3d.player.UnityPlayerActivity -lib .\build\debug\ --ndk_path $NDKPath --record_options=--exit-with-parent -r "--call-graph fp"
+echo "Using NDK Path: $NDKPath"
+python3 $NDKPath/simpleperf/app_profiler.py -p com.beatgames.beatsaber -a com.unity3d.player.UnityPlayerActivity -lib ./build/debug/ --ndk_path $NDKPath --record_options=--exit-with-parent -r "--call-graph fp"
 
 if ($?) {
-    python3.10 $NDKPath/simpleperf/report_html.py
+    python3 $NDKPath/simpleperf/report_html.py
 }
